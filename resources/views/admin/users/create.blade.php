5bdb0f5d9e489d63569012c67844d4bb1008f903
ismaila sylla <syldox@gmail.com>
2017-09-01 03:14:54 +0530

    18-Populating the user roles select

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -6,16 +6,32 @@
 {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store']) !!}
 
                     <div class="form-group">
-
-                        {!! Form::label('title','Title:') !!}
-                        {!! Form::text('title', null, ['class'=>'form-control']) !!}
-
+                        {!! Form::label('name','Name:') !!}
+                        {!! Form::text('name', null, ['class'=>'form-control']) !!}
                     </div>
 
 
                     <div class="form-group">
+                        {!! Form::label('email','Email:') !!}
+                        {!! Form::email('email', null, ['class'=>'form-control']) !!}
+                    </div>
 
-                        {!! Form::submit('Create Post', ['class'=>'btn btn-primary']) !!}
+
+                    <div class="form-group">
+                        {!! Form::label('role_id','Role:') !!}
+                        {!! Form::select('role_id',[''=>'Chose Options'] + $roles, null, ['class'=>'form-control']) !!}
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::label('status','Status:') !!}
+                        {!! Form::select('status',array(1=>'Active',0=>'Not Active') ,null, ['class'=>'form-control']) !!}
+                    </div>
+
+
+
+    <div class="form-group">
+
+                        {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
 
                     </div>
 
