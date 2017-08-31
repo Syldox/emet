da3755ffc5573f5190a47608f605f6a29ce3547e
ismaila sylla <syldox@gmail.com>
2017-09-01 03:50:02 +0530

    21-Adding upload file feature to form

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,44 +1,72 @@
 @extends('layouts.admin')
+
+
+
 @section('content')
+
 
     <h1>Create Users</h1>
 
-{!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store']) !!}
 
-                    <div class="form-group">
-                        {!! Form::label('name','Name:') !!}
-                        {!! Form::text('name', null, ['class'=>'form-control']) !!}
-                    </div>
+    {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store','files'=>true]) !!}
 
+    {!! csrf_field() !!}
 
-                    <div class="form-group">
-                        {!! Form::label('email','Email:') !!}
-                        {!! Form::email('email', null, ['class'=>'form-control']) !!}
-                    </div>
+    <div class="form-group">
+        {!! Form::label('name','Name:') !!}
+        {!! Form::text('name', null, ['class'=>'form-control']) !!}
+    </div>
 
 
-                    <div class="form-group">
-                        {!! Form::label('role_id','Role:') !!}
-                        {!! Form::select('role_id',[''=>'Chose Options'] + $roles, null, ['class'=>'form-control']) !!}
-                    </div>
+    <div class="form-group">
+        {!! Form::label('email','Email:') !!}
+        {!! Form::email('email', null, ['class'=>'form-control']) !!}
+    </div>
 
-                    <div class="form-group">
-                        {!! Form::label('status','Status:') !!}
-                        {!! Form::select('status',array(1=>'Active',0=>'Not Active') ,null, ['class'=>'form-control']) !!}
-                    </div>
+
+    <div class="form-group">
+        {!! Form::label('role_id','Role:') !!}
+        {!! Form::select('role_id', [''=>'Choose Options'] + $roles , null, ['class'=>'form-control']) !!}
+    </div>
+
+
+    <div class="form-group">
+
+        {!! Form::label('is_active','Status:') !!}
+        {!! Form::select('is_active', array(1 =>'Active', 0=>'Not Active'), null, ['class'=>'form-control']) !!}
+
+    </div>
+
+
+    <div class="form-group">
+
+        {!! Form::label('photo_id','Photo:') !!}
+        {!! Form::file('photo_id', ['class'=>'form-control']) !!}
+    </div>
 
 
 
     <div class="form-group">
 
-                        {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
-
-                    </div>
-
-
-{!! Form::close() !!}
+        {!! Form::label('password','Password:') !!}
+        {!! Form::password('password', ['class'=>'form-control']) !!}
+    </div>
 
 
+    <div class="form-group">
+        {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+    </div>
 
-@stop
 
+
+
+    {!! Form::close() !!}
+
+    {{--form on submit  Error code --}}
+
+
+    @include('includes.form-error')
+
+
+
+@endsection
