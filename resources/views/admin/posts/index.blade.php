cb14d2ed43a8873a6400f78876f41547ef8ef5dc
ismaila sylla <syldox@gmail.com>
2017-09-01 22:22:30 +0530

    3-Displaying post

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -7,6 +7,38 @@
 
     <h1>Users Posts</h1>
 
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>User</th>
+            <th>Category</th>
+            <th>Photo</th>
+            <th>title</th>
+            <th>body</th>
+            <th>Created</th>
+            <th>Updated</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @if($posts)
+            @foreach($posts as $post)
+                <tr>
+                    <td>{{$post->id}}</td>
+                    <td>{{$post->user_id}}</td>
+                    <td>{{$post->category_id}}</td>
+                    <td>{{$post->photo_id}}</td>
+                    <td>{{$post->title}}</td>
+                    <td>{{$post->body}}</td>
+                    <td>{{$post->created_at->difForHumans()}}</td>
+                    <td>{{$post->updated_at->difForHumans()}}</td>
+                </tr>
+            @endforeach
+        @endif
+
+        </tbody>
+    </table>
 
 
 
