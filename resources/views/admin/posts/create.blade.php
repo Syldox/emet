281a0b6e5bb921c16d838fe69b3da615efc6ebd9
ismaila sylla <syldox@gmail.com>
2017-09-01 23:02:23 +0530

    5-Creating form part 1

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -8,6 +8,47 @@
     <h1>Create Posts</h1>
 
 
+    {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store']) !!}
+
+            <div class="form-group">
+
+                {!! Form::label('title','Title:') !!}
+                {!! Form::text('title', null, ['class'=>'form-control']) !!}
+
+            </div>
+
+            <div class="form-group">
+
+                {!! Form::label('category_id','Category:') !!}
+                {!! Form::select('category_id',array(''=>'options'), null, ['class'=>'form-control']) !!}
+
+            </div>
+
+            <div class="form-group">
+
+                {!! Form::label('photo_id','Photo:') !!}
+                {!! Form::file('photo_id', ['class'=>'form-control']) !!}
+
+            </div>
+
+
+
+            <div class="form-group">
+
+                {!! Form::label('body','Description:') !!}
+                {!! Form::textarea('body', null, ['class'=>'form-control']) !!}
+
+            </div>
+
+
+            <div class="form-group">
+
+                {!! Form::submit('Create Post', ['class'=>'btn btn-primary']) !!}
+
+            </div>
+
+
+    {!! Form::close() !!}
 
 
 
