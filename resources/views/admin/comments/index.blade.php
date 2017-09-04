49f7fa11edda2a3d2c93a1d6794264be7686d6dc
ismaila sylla <syldox@gmail.com>
2017-09-05 02:08:57 +0530

    displaying-posts

--- resources/views/admin/comments/index.blade.php
+++ resources/views/admin/comments/index.blade.php
@@ -1,6 +1,43 @@
 @extends('layouts.admin')
 @section('content')
 
-<h1>Comments</h1>
 
+
+<div class="well">
+    @if(count($comments)>0)
+
+        <h1>Comments</h1>
+
+        <table class="table">
+            <thead>
+            <tr>
+                <th>id</th>
+                <th>Author</th>
+                <th>Email</th>
+                <th>Body</th>
+                <th>Post</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            @foreach($comments as $comment)
+                <tr>
+                    <td>{{$comment->id}}</td>
+                    <td>{{$comment->author}}</td>
+                    <td>{{$comment->email}}</td>
+                    <td>{{$comment->body}}</td>
+                    <td><a href="{{route('home.post',$comment->post->id)}}"><span class="label label-info">View Post</span></a></td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+
+    @else
+
+        <h1 class="text-center"><span class="label label-warning" >No Comments</span></h1>
+
+
+    @endif
+
+</div>
     @stop
