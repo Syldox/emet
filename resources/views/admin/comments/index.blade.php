842b9c3e9e44e3ddde1e687634254413ef2fef4b
ismaila sylla <syldox@gmail.com>
2017-09-05 14:06:34 +0530

    approving-unapproving-and-deleting-part-1-setting-it-up

--- resources/views/admin/comments/index.blade.php
+++ resources/views/admin/comments/index.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.admin')
 @section('content')
 
-
-
 <div class="well">
     @if(count($comments)>0)
 
@@ -15,7 +13,7 @@
                 <th>Author</th>
                 <th>Email</th>
                 <th>Body</th>
-                <th>Post</th>
+                <th>Posts</th>
             </tr>
             </thead>
             <tbody>
@@ -27,6 +25,57 @@
                     <td>{{$comment->email}}</td>
                     <td>{{$comment->body}}</td>
                     <td><a href="{{route('home.post',$comment->post->id)}}"><span class="label label-info">View Post</span></a></td>
+
+                   <td>
+
+                       @if($comment->is_active == 1)
+
+                           {!! Form::open(['method'=>'PATCH','action'=>['PostCommentsController@update',$comment->id]]) !!}
+
+                                <input type="hidden" name="is_active" value="0">
+
+                           <div class="form-group">
+
+                               {!! Form::submit('Un-approve', ['class'=>'btn btn-info']) !!}
+
+                           </div>
+
+                           {!! Form::close() !!}
+
+                       @else
+
+                           {!! Form::open(['method'=>'PATCH','action'=>['PostCommentsController@update',$comment->id]]) !!}
+                                <input type="hidden" name="is_active" value="1">
+
+                           <div class="form-group">
+
+                               {!! Form::submit('Aprove', ['class'=>'btn btn-success']) !!}
+
+                           </div>
+
+                           {!! Form::close() !!}
+
+                       @endif
+
+                   </td>
+
+
+
+                    <td>
+                        {!! Form::open(['method'=>'DELETE','action'=>['PostCommentsController@destroy',$comment->id]]) !!}
+
+                        <div class="form-group">
+
+                            {!! Form::submit('Delete', ['class'=>'btn btn-danger']) !!}
+
+                        </div>
+
+                        {!! Form::close() !!}
+
+                    </td>
+
+
+
                 </tr>
             @endforeach
             </tbody>
