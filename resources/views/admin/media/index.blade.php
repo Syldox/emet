243cc0253e9192675e6f739c32298f641990289a
ismaila sylla <syldox@gmail.com>
2017-09-09 16:01:50 +0530

    291-part-3-adding-javascript-jquery

--- resources/views/admin/media/index.blade.php
+++ resources/views/admin/media/index.blade.php
@@ -66,9 +66,38 @@
 
 
        @endif
+
    </div>
 
     </form>
-
-
 @endsection
+
+@section('scripts')
+    <script>
+        $(document).ready(function(){
+
+           $('#options').click(function () {
+                if(this.checked){
+
+                    $('.checkBoxes').each(function () {
+
+                        this.checked = true;
+
+                    })
+                }else{
+
+                    $('.checkBoxes').each(function () {
+
+                        this.checked = false;
+
+                    })
+
+                }
+
+           });
+
+        });
+    </script>
+
+
+@stop
