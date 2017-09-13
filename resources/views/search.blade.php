734c09eddf7ba419ea179043fcd89e7479464ecb
ismaila sylla <syldox@gmail.com>
2017-09-13 19:18:35 +0530

    sending mailling configuration

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 
 
-    <h1 class="text-center">Your Search Query was
+    <h1 class="text-center"> Search Results!
 
 
         <br>
 
         <small>
-            {{$query}}
+           <b>For:</b> {{$query}}
         </small>
 
     </h1>
@@ -21,18 +21,28 @@
 
             @foreach($details as $post )
 
-                <div class=".panel panel-default">
+                <div class="panel panel-default">
 
                     <div class="panel-body">
 
-                        <img src="{{$post->photo->file}}" alt="" class="img-responsive pull-left">
-                        <h4 class="pull-left" style="margin-left: 20px">{{$post->title}}</h4>
+                        <div class="row">
+                            <div class="col-md-8 col-md-offset-2">
+                                <!-- Preview Image -->
+                                <img class="form-spacing-top img-responsive"  height="200" width="650" alt=""  src="{{$post->photo ? $post->photo->file : $post->photoPlaceHolder() }}" alt="" >
+                                {{--<img src="{{asset('images/' . $post->image)}}" height="200" width="500" alt="" class="form-spacing-top">--}}
+                                <h2>{{$post->title}}</h2>
+                                <h5>Published: {{date('M j, Y', strtotime($post->created_at))}}</h5>
+                                <p>{{ substr(strip_tags($post->body),0,250)}}{{strlen(strip_tags($post->body)) > 250 ? '...' : ""}}</p>
+                                <a href="{{url('blog/'.$post->slug)}}" class="btn btn-warning btn-orange">Read More</a>
 
+                            </div>
+                        </div>
 
                     </div>
 
-
                 </div>
+
+                <hr style="padding-top: 10px; margin-left: -70px">
 
             @endforeach
 
@@ -40,7 +50,7 @@
 
             <h2 class="text-center">
 
-                No Search Results!
+                :-/ Ops Sorry No Search Results!
 
             </h2>
 
