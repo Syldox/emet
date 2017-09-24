b0c2bb0e0ac5aac59bd0715db88bbb6418dda2cc
ismaila sylla <syldox@gmail.com>
2017-09-24 19:11:10 +0530

    fixing  position adding Styles to the contact and About section

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,146 +2,143 @@
 @section('title', '| About')
 
 @section('content')
+    <div class="container" style="margin-left: -100px;margin-top: -50px">
 
+        <h1>Orange Map </h1>
 
+        <div id="map"></div>
 
-
-    <h1>Orange Map </h1>
-
-    <div id="map"></div>
-
-    <script>
-        function initMap() {
+        <script>
+            function initMap() {
 
 //            Map Options
 
-            var options ={
+                var options ={
 
-                zoom:8,
-                center:{lat:12.6500000,lng:-8.0000000}
-            };
+                    zoom:8,
+                    center:{lat:12.6500000,lng:-8.0000000}
+                };
 
 //            New Map
-            var map = new
-            google.maps.Map(document.getElementById('map'),options);
+                var map = new
+                google.maps.Map(document.getElementById('map'),options);
 
-            //listen map on click
+                //listen map on click
 
-            google.maps.event.addListener(map, 'click', function(event){
+                google.maps.event.addListener(map, 'click', function(event){
 
 //                add marker
-                addMarker({coords:event.latLng});
+                    addMarker({coords:event.latLng});
 
 
-            });
+                });
 //
+                //Array of Markers
 
-////            Adding Marker
-
-//            var marker = new google.maps.Marker({
-//                position:{lat:12.633866 , lng:-8.024445},
-//                map:map
-////                icon:'http://www.clker.com/cliparts/I/l/L/S/W/9/map-marker-hi.png'
-//
-//            });
-
-//
-//            var infoWindow = new google.maps.InfoWindow({
-//               content:'<h1>Orange Mali Aci 2000</h1>'
-//
-//            });
-//
-//            marker.addListener('click',function(){
-//
-//                infoWindow.open(map,marker);
-//            });
+                var markers =[
 
 
+                    {
+                        coords:{lat:12.630435 , lng: -8.028238},
+                        iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+                        content:'<h1>Orange Mali</h1>'
+                    },
 
 
-
-
-            //Array of Markers
-
-            var markers =[
-
-
-                {
-                    coords:{lat:12.630435 , lng: -8.028238},
-                    iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-                    content:'<h1>Orange Mali</h1>'
-                },
-
-
-                {
-                    coords:{lat:12.633866 , lng:-8.024445},
-                    content:'<h1>Orange Mall</h1>'
+                    {
+                        coords:{lat:12.633866 , lng:-8.024445},
+                        content:'<h1>Orange Mall</h1>'
 //                iconImage:'http://www.clker.com/cliparts/I/l/L/S/W/9/map-marker-hi.png'
+                    }
+
+                ];
+
+
+                //loop through markers
+
+                for (var i = 0; i < markers.length; i++){
+
+                    //            add marker location to the map
+                    addMarker(markers[i]);
+
+
                 }
 
-            ];
+
+                //add marker function
 
 
-            //loop through markers
-
-            for (var i = 0; i < markers.length; i++){
-
-                //            add marker location to the map
-                addMarker(markers[i]);
-
-
-            }
-
-
-            //add marker function
-
-
-            function addMarker(props){
-                var marker = new google.maps.Marker({
-                position:props.coords,
-                map:map
+                function addMarker(props){
+                    var marker = new google.maps.Marker({
+                        position:props.coords,
+                        map:map
 //                icon:props.iconImage
 //                icon:'http://www.clker.com/cliparts/I/l/L/S/W/9/map-marker-hi.png'
 //
-            });
+                    });
 
 //                check for custom icon
-                if(props.iconImage){
+                    if(props.iconImage){
 
 //                    set icon image
 
-                    marker.setIcon(props.iconImage);
+                        marker.setIcon(props.iconImage);
+
+                    }
+
+
+                    //check content
+
+                    if(props.content){
+
+
+                        var infoWindow = new google.maps.InfoWindow({
+                            content:props.content
+
+                        });
+
+                        marker.addListener('click',function(){
+
+                            infoWindow.open(map,marker);
+                        });
+
+                    }
 
                 }
 
 
-                //check content
-
-                if(props.content){
-
-
-            var infoWindow = new google.maps.InfoWindow({
-               content:props.content
-
-            });
-
-            marker.addListener('click',function(){
-
-                infoWindow.open(map,marker);
-            });
-
-                }
 
             }
+        </script>
+
+        <script async defer
+                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDGkrBAgjF9LYMsVpgWyVZ_PhHWULDdpng&callback=initMap">
+        </script>
+
+        <hr>
+
+
+        {{--side info --}}
+
+        <div class="col-md-4">
+            <form>
+                <legend><span class="glyphicon glyphicon-globe"></span> Our office</legend>
+                <address>
+                    <strong>Twitter, https://twitter.com/Orange_Mali</strong><br>
+                    Orange Mali Aci 2000<br>
+                    Bamako, Mali<abbr title="Phone">
+                        P:</abbr>
+                    +223 44 99 99 03
+                </address>
+                <address>
+                    <a href="mailto:#">OrangeMali@gmail.com</a>
+                </address>
+            </form>
+        </div>
 
 
 
-        }
-    </script>
-
-    <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDGkrBAgjF9LYMsVpgWyVZ_PhHWULDdpng&callback=initMap">
-    </script>
+    </div>
 
 
 @endsection
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -3,94 +3,191 @@
 
 @section('content')
 
-    <h1>Contact Me</h1>
-    <hr>
-    <div class="container col-md-12">
-        <div class="row">
-            <div class="col-md-8">
-                <div class="well well-sm">
-                    <form action="{{url('contact')}}" method="POST">
-                        {{csrf_field()}}
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="email">
-                                        Email Address</label>
-                                    <div class="input-group">
+   <div class="container" style="margin-left: -100px;margin-top: -50px">
+       <h1>Contactz Orange Mali </h1>
+       <hr>
+
+       <div class="container">
+           <img src="../images/orange-ico.jpg" alt="">
+       </div>
+
+       <div class="container col-md-12"  style="margin-top: -100px">
+           <div class="row">
+               <div class="col-md-8" style="margin-top: 133px">
+                   <div class="well well-sm">
+                       <form action="{{url('contact')}}" method="POST">
+                           {{csrf_field()}}
+                           <div class="row">
+                               <div class="col-md-6">
+                                   <div class="form-group">
+                                       <label for="email">
+                                           Email Address</label>
+                                       <div class="input-group">
                     <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
                     </span>
-                                        <input type="email" class="form-control" id="email" placeholder="Enter email" required="required" name="email"/></div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="subject">
-                                        Subject</label>
-                                    <select id="subject" name="subject" class="form-control" required="required">
-                                        <option value="na" selected="">Choose One:</option>
-                                        <option value="service">General Customer Service</option>
-                                        <option value="suggestions">Suggestions</option>
-                                        <option value="product">Product Support</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="name">
-                                        Message</label>
-                                    <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
-                                              placeholder="Message"></textarea>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <button type="submit" class="btn btn-warning pull-right" id="btnContactUs" style="background-color: #f5861a">
-                                    Send Message</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+                                           <input type="email" class="form-control" id="email" placeholder="Enter email" required="required" name="email"/></div>
+                                   </div>
+                                   <div class="form-group">
+                                       <label for="subject">
+                                           Subject</label>
+                                       <select id="subject" name="subject" class="form-control" required="required">
+                                           <option value="na" selected="">Choose One:</option>
+                                           <option value="service">General Customer Service</option>
+                                           <option value="suggestions">Suggestions</option>
+                                           <option value="product">Product Support</option>
+                                       </select>
+                                   </div>
+                               </div>
+                               <div class="col-md-6">
+                                   <div class="form-group">
+                                       <label for="name">
+                                           Message</label>
+                                       <textarea name="message" id="message" class="form-control" rows="9" cols="25" required="required"
+                                                 placeholder="Message"></textarea>
+                                   </div>
+                               </div>
+                               <div class="col-md-12">
+                                   <button type="submit" class="btn btn-warning pull-right" id="btnContactUs" style="background-color: #f5861a">
+                                       Send Message</button>
+                               </div>
+                           </div>
+                       </form>
+                   </div>
+               </div>
 
 
-            <div class="col-md-4">
-                <form>
-                    <legend><span class="glyphicon glyphicon-globe"></span> Our office</legend>
-                    <address>
-                        <strong>Twitter, https://twitter.com/Orange_Mali</strong><br>
-                        Orange Mali Aci 2000<br>
-                        Bamako, Mali<abbr title="Phone">
-                            P:</abbr>
-                        +223 44 99 99 03
-                    </address>
-                    <address>
-                        <a href="mailto:#">OrangeMali@gmail.com</a>
-                    </address>
-                </form>
-            </div>
-        </div>
-    </div>
+               <div class="col-md-4" style="margin-top: -100px">
+                   <form>
+                       <legend><span class="glyphicon glyphicon-globe"></span> Our office</legend>
+                       <address>
+                           <strong>Twitter, https://twitter.com/Orange_Mali</strong><br>
+                           Orange Mali Aci 2000<br>
+                           Bamako, Mali<abbr title="Phone">
+                               P:</abbr>
+                           +223 44 99 99 03
+                       </address>
+                       <address>
+                           <a href="mailto:#">OrangeMali@gmail.com</a>
+                       </address>
+                   </form>
+
+                   {{--Google Maps --}}
+
+                   <h1>Orange Map </h1>
+
+                   <div id="map" style="width: 500px;height: 288px ; margin-top: 22px" ></div>
+
+                   <script>
+                       function initMap() {
+
+//            Map Options
+
+                           var options ={
+
+                               zoom:8,
+                               center:{lat:12.6500000,lng:-8.0000000}
+                           };
+
+//            New Map
+                           var map = new
+                           google.maps.Map(document.getElementById('map'),options);
+
+                           //listen map on click
+
+                           google.maps.event.addListener(map, 'click', function(event){
+
+//                add marker
+                               addMarker({coords:event.latLng});
+
+
+                           });
+//
+                           //Array of Markers
+
+                           var markers =[
+
+
+                               {
+                                   coords:{lat:12.630435 , lng: -8.028238},
+                                   iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+                                   content:'<h1>Orange Mali</h1>'
+                               },
+
+
+                               {
+                                   coords:{lat:12.633866 , lng:-8.024445},
+                                   content:'<h1>Orange Mall</h1>'
+//                iconImage:'http://www.clker.com/cliparts/I/l/L/S/W/9/map-marker-hi.png'
+                               }
+
+                           ];
+
+
+                           //loop through markers
+
+                           for (var i = 0; i < markers.length; i++){
+
+                               //            add marker location to the map
+                               addMarker(markers[i]);
+
+
+                           }
+
+
+                           //add marker function
+
+
+                           function addMarker(props){
+                               var marker = new google.maps.Marker({
+                                   position:props.coords,
+                                   map:map
+//                icon:props.iconImage
+//                icon:'http://www.clker.com/cliparts/I/l/L/S/W/9/map-marker-hi.png'
+//
+                               });
+
+//                check for custom icon
+                               if(props.iconImage){
+
+//                    set icon image
+
+                                   marker.setIcon(props.iconImage);
+
+                               }
+
+
+                               //check content
+
+                               if(props.content){
+
+
+                                   var infoWindow = new google.maps.InfoWindow({
+                                       content:props.content
+
+                                   });
+
+                                   marker.addListener('click',function(){
+
+                                       infoWindow.open(map,marker);
+                                   });
+
+                               }
+
+                           }
 
 
 
-    <h1>Google Map</h1>
+                       }
+                   </script>
 
-    <div id="map"></div>
+                   <script async defer
+                           src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDGkrBAgjF9LYMsVpgWyVZ_PhHWULDdpng&callback=initMap">
+                   </script>
 
-    <script>
-        function initMap() {
-
-            var options ={
-
-                zoom:8,
-                center:{lat:12.6500000,lng:-8.0000000}
-            }
-
-            var map = new
-            google.maps.Map(document.getElementById('map'),options);
+               </div>
+           </div>
+       </div>
 
 
-        }
-    </script>
-
-    <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDGkrBAgjF9LYMsVpgWyVZ_PhHWULDdpng&callback=initMap">
-    </script>
+   </div>
 @endsection
